8807be008e0f88fbbf2abe4747d21e188432dc92
NAre5 <sherlord5@gmail.com>
2020-06-11 20:40:59 +0300

    {

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/*",
+    component: NotFound
+  },
   {
     path: "/",
     name: "Home",
