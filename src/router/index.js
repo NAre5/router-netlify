660ea728b1a227ee62203503bc623152c756eb83
NAre5 <sherlord5@gmail.com>
2020-06-11 20:43:14 +0300

    NotFound

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/*",
-    component: NotFound
-  },
-  {
     path: "/",
     name: "Home",
     component: Home
@@ -23,6 +19,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/*",
+    component: NotFound
   }
 ];
 
